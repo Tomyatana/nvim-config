ff6373a2cbe17ff4fff2466730fc5773136c2a83
Tomás Atanasoff <tomyatana@gmail.com>
2026-04-14 07:44:33 -0300

    removed unecessary plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,10 +3,4 @@ return {
         "eandrju/cellular-automaton.nvim",
         lazy = false
     },
-    {
-        "Tomyatana/nvim-gf-fix",
-        config = function ()
-            require("nvim-gf-fix").setup({})
-        end
-    }
 }
